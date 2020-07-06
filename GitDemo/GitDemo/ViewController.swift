8a51610c6057fa20efcb240533ec189e3fd7ecd2
Anjali-RightPoint <aaggarwal@rightpoint.com>
2020-07-06 16:56:35 +0530

    Fourth Commit

--- GitDemo/GitDemo/ViewController.swift
+++ GitDemo/GitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.red
+        print("The colors are nice")
         // Do any additional setup after loading the view.
     }
 
