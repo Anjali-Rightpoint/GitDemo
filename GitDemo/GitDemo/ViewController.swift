7c5f94ea330856562a5e9e6c572caf73130d063d
Anjali-RightPoint <aaggarwal@rightpoint.com>
2020-07-06 15:41:34 +0530

    Initial Commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Anjali Aggarwal on 06/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
