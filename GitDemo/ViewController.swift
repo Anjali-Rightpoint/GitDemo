eedd8d260de8fb40b0b604ec4f585504a89a23d6
Anjali-RightPoint <aaggarwal@rightpoint.com>
2020-07-08 20:16:25 +0530

    squashed commit

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -11,10 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Commit 1
+        print("This is commit 1 to be squashed")
         self.view.backgroundColor = UIColor.red
         print("The colors are nice")
         self.view.backgroundColor = UIColor.blue
         print("Conflict should come here")
+        
+        // Commit 2
+        print("This is commit 2 to be squashed")
+        
         self.view.backgroundColor = UIColor.green
         print("The colors are nice")
         // Do any additional setup after loading the view.
